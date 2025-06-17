37ff7187160bafe49393243e9473119899d56dc8
alitolga0 <cakiralitolga@gmail.com>
2025-06-17 17:13:12 +0300

    feat : added services

---
+++ ECommerceRestApi/Core/Service/IBaseService.cs
@@ -0,0 +1,16 @@
+﻿using ECommerceRestApi.Core.Utilities.Result;
+using IResult = ECommerceRestApi.Core.Utilities.Result.IResult;
+using System.Linq.Expressions;
+
+namespace ECommerceRestApi.Core.Service
+{
+    public interface IBaseService<TEntity, TKey> where TEntity : class
+    {
+        Task<IResult> Add(TEntity entity);
+        Task<IResult> Update(TEntity entity);
+        Task<IResult> Delete(TKey id);
+        IDataResult<List<TEntity>> GetAll(Expression<Func<TEntity, bool>> filter = null);
+        IDataResult<TEntity> GetById(TKey id);
+    }
+
+}
---
+++ ECommerceRestApi/Services/Concrete/OrderService.cs
@@ -0,0 +1,70 @@
+﻿using ECommerceRestApi.Core.Repository;
+using ECommerceRestApi.Core.Utilities.Result;
+using ECommerceRestApi.Models;
+using ECommerceRestApi.Services.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using IResult = ECommerceRestApi.Core.Utilities.Result.IResult;
+namespace ECommerceRestApi.Services.Concrete
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IBaseRepository<Order> _repository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderService(IBaseRepository<Order> repository, IUnitOfWork unitOfWork)
+        {
+            _repository = repository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IResult> Add(Order entity)
+        {
+         
+            // 1. OrderDate ve Status ayarla
+            entity.OrderDate = DateTime.UtcNow;
+            entity.Status = OrderStatus.Pending;
+
+            // 2. Toplam tutarı hesapla
+            entity.TotalAmount = entity.OrderItems.Sum(oi => oi.UnitPrice * oi.Quantity);
+
+            // 3. Önce order ekle
+            await _repository.Add(entity);
+            await _unitOfWork.SaveChangesAsync();
+
+            // Burada EF Core OrderId'yi otomatik atar ve OrderItem'lar buna bağlanır
+
+            return new SuccessResult("sipariş başarıyla eklendi");
+        }
+
+  
+
+        public async Task<IResult> Delete(Guid id)
+        {
+            await _repository.Delete(id);
+            await _unitOfWork.SaveChangesAsync();
+            return new SuccessResult("sipariş başarıyla silindi");
+        }
+
+        public IDataResult<List<Order>> GetAll(Expression<Func<Order, bool>> filter = null)
+        {
+            var data = _repository.GetAll(filter);
+            return new SuccessDataResult<List<Order>>(data,"siparişler başarıyla getirildi");
+        }
+
+        public IDataResult<Order> GetById(Guid id)
+        {
+            var data = _repository.Get(x=> x.Id == id);
+            return new SuccessDataResult<Order>(data, "Order getirildi.");
+        }
+
+        public async Task<IResult> Update(Order entity)
+        {
+            await _repository.Update(entity);
+            await _unitOfWork.SaveChangesAsync();
+            return new SuccessResult("sipariş başarıyla güncellendi");
+        }
+    }
+}
