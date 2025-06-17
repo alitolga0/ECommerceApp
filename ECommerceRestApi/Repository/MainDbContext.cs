ae248e5a5ea6c641db35abf315fa75f581adfb53
alitolga0 <cakiralitolga@gmail.com>
2025-06-17 17:12:49 +0300

    create project

---
+++ ECommerceRestApi/Repository/MainDbContext.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ECommerceRestApi.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace ECommerceRestApi.Repository
+{
+    public class MainDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
+    {
+        public MainDbContext(DbContextOptions<MainDbContext> options)
+            : base(options) { }
+
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<Category> Categories => Set<Category>();
+        public DbSet<CartItem> CartItems => Set<CartItem>();
+        public DbSet<Order> Orders => Set<Order>();
+        public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Identity tablolarının isimlerini değiştirelim
+            builder.Entity<User>(b =>
+            {
+                b.ToTable("Users");
+                b.HasIndex(u => u.Email).IsUnique();
+            });
+            builder.Entity<IdentityRole<Guid>>(b =>
+            {
+                b.ToTable("Roles");
+            });
+            builder.Entity<IdentityUserRole<Guid>>(b =>
+            {
+                b.ToTable("UserRoles");
+            });
+            builder.Entity<IdentityUserClaim<Guid>>(b =>
+            {
+                b.ToTable("UserClaims");
+            });
+            builder.Entity<IdentityUserLogin<Guid>>(b =>
+            {
+                b.ToTable("UserLogins");
+            });
+            builder.Entity<IdentityRoleClaim<Guid>>(b =>
+            {
+                b.ToTable("RoleClaims");
+            });
+            builder.Entity<IdentityUserToken<Guid>>(b =>
+            {
+                b.ToTable("UserTokens");
+            });
+
+            
+            builder.Entity<Product>()
+                .HasOne(p => p.User)
+                .WithMany(u => u.Products)
+                .HasForeignKey(p => p.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+       
+            builder.Entity<CartItem>()
+                .HasOne(ci => ci.User)
+                .WithMany(u => u.CartItems)
+                .HasForeignKey(ci => ci.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Order>()
+                .HasOne(o => o.User)
+                .WithMany(u => u.Orders)
+                .HasForeignKey(o => o.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Product>()
+                .HasOne(p => p.Category)
+                .WithMany(c => c.Products)
+                .HasForeignKey(p => p.CategoryId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+           
+            builder.Entity<CartItem>()
+                .HasOne(ci => ci.Product)
+                .WithMany(p => p.CartItems)
+                .HasForeignKey(ci => ci.ProductId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // OrderItem - Order
+            builder.Entity<OrderItem>()
+                .HasOne(oi => oi.Order)
+                .WithMany(o => o.OrderItems)
+                .HasForeignKey(oi => oi.OrderId)
+                .OnDelete(DeleteBehavior.Cascade);  
+
+            builder.Entity<OrderItem>()
+                .HasOne(oi => oi.Product)
+                .WithMany(p => p.OrderItems)
+                .HasForeignKey(oi => oi.ProductId)
+                .OnDelete(DeleteBehavior.Restrict); 
+
+            builder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasPrecision(18, 2);
+
+            builder.Entity<OrderItem>()
+                .Property(oi => oi.UnitPrice)
+                .HasPrecision(18, 2);
+        }
+
+    }
+}
