fbb930db3cc51c95ee892b1d4e72c511e99209e3
alitolga0 <cakiralitolga@gmail.com>
2025-06-18 14:21:30 +0300

    feat : added user service

---
+++ ECommerceRestApi/Services/Abstract/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using ECommerceRestApi.Core.Service;
+using ECommerceRestApi.Core.Utilities.Result;
+using ECommerceRestApi.Models;
+using System.Linq.Expressions;
+using IResult = ECommerceRestApi.Core.Utilities.Result.IResult;
+namespace ECommerceRestApi.Services.Abstract
+{
+    public interface IUserService 
+    {
+       
+        IDataResult<User> GetById(Guid id);
+        IDataResult<List<User>> GetAll(Expression<Func<User, bool>>? filter = null);
+       
+    }
+}
---
+++ ECommerceRestApi/Services/Concrete/UserService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ECommerceRestApi.Models;
+using ECommerceRestApi.Services.Abstract;
+using ECommerceRestApi.Core.Utilities.Result;
+using System.Linq.Expressions;
+using IResult = ECommerceRestApi.Core.Utilities.Result.IResult;
+using ECommerceRestApi.Core.Repository;
+
+namespace ECommerceRestApi.Services.Concrete
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserService(UserManager<User> userManager, IUnitOfWork unitOfWork)
+        {
+            _userManager = userManager;
+            _unitOfWork = unitOfWork;
+        }
+
+     
+        public IDataResult<User> GetById(Guid id)
+        {
+            var user = _userManager.Users.FirstOrDefault(u => u.Id == id);
+            return new SuccessDataResult<User>(user,"başarıyla listelendi");
+        }
+
+        public IDataResult<List<User>> GetAll(Expression<Func<User, bool>>? filter = null)
+        {
+            var query = _userManager.Users;
+            var users = filter != null ? query.Where(filter).ToList() : query.ToList();
+            return new SuccessDataResult<List<User>>(users ,"başarıyla listelendi");
+        }
+
+       
+    }
+}
